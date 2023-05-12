2e7acbea7b8fc1b4f25447d0b0a77039e83edcf2
Muhammad Suleman <m.suleman275@gmail.com>
2023-05-12 17:52:20 +0300

    solved and submitted

--- java/log-levels/src/main/java/LogLevels.java
+++ java/log-levels/src/main/java/LogLevels.java
@@ -1,14 +1,26 @@
 public class LogLevels {
     
     public static String message(String logLine) {
-        throw new UnsupportedOperationException("Please implement the (static) LogLine.message() method");
+        String[] words = logLine.trim().split(" ");
+        String message = "";
+
+        for (String string : words) {
+            if (!string.isBlank() && !string.contains(":")) {
+               message += string + " ";
+            }
+         }
+         
+        return message.trim();
     }
 
     public static String logLevel(String logLine) {
-        throw new UnsupportedOperationException("Please implement the (static) LogLine.logLevel() method");
+        String[] words = logLine.trim().split(" ");
+        String target = words[0];
+
+        return target.substring(1, target.length()-2).toLowerCase();
     }
 
     public static String reformat(String logLine) {
-        throw new UnsupportedOperationException("Please implement the (static) LogLine.reformat() method");
+        return message(logLine) + " (" + logLevel(logLine) + ")";
     }
 }
