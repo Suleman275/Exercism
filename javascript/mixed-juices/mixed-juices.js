c795cf27a696a1236fd48d651526be8144f72833
Muhammad Suleman <m.suleman275@gmail.com>
2023-06-13 12:38:59 +0300

    solved

--- javascript/mixed-juices/mixed-juices.js
+++ javascript/mixed-juices/mixed-juices.js
@@ -4,6 +4,8 @@
 // the @ts-check directive. It will give you helpful autocompletion when
 // implementing this exercise.
 
+import { time } from "console";
+
 /**
  * Determines how long it takes to prepare a certain juice.
  *
@@ -11,7 +13,21 @@
  * @returns {number} time in minutes
  */
 export function timeToMixJuice(name) {
-  throw new Error('Please implement the timeToMixJuice function');
+  switch (name) {
+    case 'Pure Strawberry Joy':
+      return 0.5;
+    case 'Energizer':
+      return 1.5;
+    case 'Green Garden':
+      return 1.5;
+    case 'Tropical Island':
+      return 3;
+    case 'All or Nothing':
+      return 5;
+    default:
+        return 2.5;
+
+  }
 }
 
 /**
@@ -23,7 +39,32 @@ export function timeToMixJuice(name) {
  * @returns {number} number of limes cut
  */
 export function limesToCut(wedgesNeeded, limes) {
-  throw new Error('Please implement the limesToCut function');
+  if (wedgesNeeded < 1) {
+    return 0;
+  }
+
+  let wedgesGot = 0;
+  let limesCut = 0;
+
+  for (let i = 0; i < limes.length; i++) {
+    if (limes[i] == 'small') {
+      wedgesGot += 6;
+    }
+    if (limes[i] == 'medium') {
+      wedgesGot += 8;
+    }
+    if (limes[i] == 'large') {
+      wedgesGot += 10;
+    }
+    
+    limesCut++;
+
+    if (wedgesGot >= wedgesNeeded) {
+      return limesCut;
+    }
+  }
+
+  return limesCut;
 }
 
 /**
@@ -34,5 +75,10 @@ export function limesToCut(wedgesNeeded, limes) {
  * @returns {string[]} remaining orders after the time is up
  */
 export function remainingOrders(timeLeft, orders) {
-  throw new Error('Please implement the remainingOrders function');
+  while (timeLeft > 0) {
+    timeLeft -= timeToMixJuice(orders[0]);
+    orders.shift()
+  }
+
+  return orders;
 }
