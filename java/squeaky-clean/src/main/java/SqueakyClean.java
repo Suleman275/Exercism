8b796035fb0b82e8a4a8676e3fa2f8ecdade863d
Muhammad Suleman <m.suleman275@gmail.com>
2023-06-16 13:21:15 +0300

    solved

--- java/squeaky-clean/src/main/java/SqueakyClean.java
+++ java/squeaky-clean/src/main/java/SqueakyClean.java
@@ -1,5 +1,29 @@
 class SqueakyClean {
     static String clean(String identifier) {
-        throw new UnsupportedOperationException("Please implement the (static) SqueakyClean.clean() method");
+        var s = new StringBuilder();
+        var dashFlag = false;
+
+        for(int i = 0; i < identifier.length(); i++){
+            char c = identifier.charAt(i);
+
+            if(c ==' '){
+                s.append('_');
+            }
+            else if(Character.isISOControl(c)){
+                s.append("CTRL");
+            }
+            else if(c == '-'){
+                dashFlag = true;
+            }
+            else if (Character.isLetter(c) && (c < 'α' || c >  'ω')) {
+                if (dashFlag) {
+                    c = Character.toUpperCase(c);
+                    dashFlag = false;
+                }
+                s.append(c);
+            }
+        }
+
+        return s.toString();
     }
 }
