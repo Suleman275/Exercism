d08791c959055c77aec3582b2139ae828779018f
Muhammad Suleman <m.suleman275@gmail.com>
2023-06-29 18:57:25 +0300

    solved

--- java/cars-assemble/src/main/java/CarsAssemble.java
+++ java/cars-assemble/src/main/java/CarsAssemble.java
@@ -1,10 +1,21 @@
 public class CarsAssemble {
 
     public double productionRatePerHour(int speed) {
-        throw new UnsupportedOperationException("Please implement the CarsAssemble.productionRateperHour() method");
+        if(speed > 9) {
+            return 221*speed*0.77;
+        }
+        else if(speed > 8) {
+            return 221*speed*0.8;
+        }
+        else if(speed > 4) {
+            return 221*speed*0.9;
+        }
+        else {
+            return 221*speed;
+        }
     }
 
     public int workingItemsPerMinute(int speed) {
-        throw new UnsupportedOperationException("Please implement the CarsAssemble.workingItemsPerMinute() method");
+        return (int) productionRatePerHour(speed)/60;
     }
 }
