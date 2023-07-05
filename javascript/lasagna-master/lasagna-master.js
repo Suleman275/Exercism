faef95bcebf7aaffe616d0674bbb9b0e8adf5647
Muhammad Suleman <m.suleman275@gmail.com>
2023-07-05 13:50:23 +0300

    progress

--- javascript/lasagna-master/lasagna-master.js
+++ javascript/lasagna-master/lasagna-master.js
@@ -10,3 +10,33 @@
  *   ...
  * }
  */
+
+export function cookingStatus(time) {
+   return time == null ? 'You forgot to set the timer.' : time == 0 ? 'Lasagna is done.' : 'Not done, please wait.'
+}
+
+export function preparationTime(layers, time=2) {
+   return layers.length * time
+}
+
+export function quantities(layers) {
+   let noodleCount = 0 
+   let sauceCount = 0;
+   layers.forEach(element => {
+      if (element == 'noodles') {
+         noodleCount+=50
+      }
+      else if (element == 'sauce') {
+         sauceCount+=0.2
+      }
+   });
+
+   return {
+      noodles: noodleCount, 
+      sauce: sauceCount
+   }
+
+   export function addSecretIngredient(friendsList, myList) {
+      
+   }
+}
